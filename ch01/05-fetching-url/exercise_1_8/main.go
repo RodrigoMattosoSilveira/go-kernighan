e605e77ffb3512b3d01c37c44769326ba120d83b
Rodrigo Silveira <r3850365@gmail.com>
2022-12-09 15:49:09 -0800

    01-tutorial-05-fetch-url Wrote exercise_1_8

---
+++ ch01/05-fetching-url/exercise_1_8/main.go
@@ -0,0 +1,29 @@
+// Prints the content at each specified URL
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	const TOKEN = "http://"
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(strings.ToLower(url), TOKEN) {
+			temp := []string{TOKEN, url}
+			url = strings.Join(temp, "")
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
