e85f750a20d192193dcd6ddb54a58ec245aa7530
Rodrigo Silveira <r3850365@gmail.com>
2022-12-27 13:30:36 -0800

    02-program-structures-06-packages-and-files reorganized

--- ch02/06-package/exercise-02-04/popcount_bench_test.go
+++ ch02/06-package/exercise-02-04/popcount_bench_test.go
@@ -6,18 +6,21 @@ import (
 	"testing"
 )
 
+// popcount
 func BenchmarkTable(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		popTable.PopCount(1023)
 	}
 }
 
+// exercise_02_03
 func BenchmarkLoop(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		popLoop.PopCount(1023)
 	}
 }
 
+// exercise_02_04
 func BenchmarkShift(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		PopCount(1023)
